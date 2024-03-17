cd840bf5ce307272049b00bd99a9f5730afcb1c3
KostiantynShtoyko <shtoykokostya7@gmail.com>
2024-03-17 15:21:40 +0200

    1.0

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print ("halo")
+import pytube
+
+# Введіть посилання на відео
+video_url = input("Введіть посилання на відео з YouTube: ")
+
+# Створюємо об'єкт YouTube
+youtube = pytube.YouTube(video_url)
+
+# Виберіть роздільну здатність відео
+video = youtube.streams.get_highest_resolution()
+
+# Виберіть шлях для збереження відео
+save_path = input("Введіть шлях для збереження відео: ")
+
+# Завантажуємо відео
+video.download(save_path)
+
+print("Відео успішно завантажено!")
