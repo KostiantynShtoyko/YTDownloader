c6db229944eaf596ac0a3fa684eefdb334da7e50
KostiantynShtoyko <shtoykokostya7@gmail.com>
2024-03-17 15:26:40 +0200

    1.1

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
-import pytube
+import tkinter as tk
+from pytube import YouTube
 
-# Введіть посилання на відео
-video_url = input("Введіть посилання на відео з YouTube: ")
+def download_video():
+    video_url = entry_url.get()
+    save_path = entry_path.get()
+    try:
+        youtube = YouTube(video_url)
+        video = youtube.streams.get_highest_resolution()
+        video.download(save_path)
+        label_status.config(text="Відео успішно завантажено!")
+    except:
+        label_status.config(text="Помилка при завантаженні відео.")
 
-# Створюємо об'єкт YouTube
-youtube = pytube.YouTube(video_url)
+root = tk.Tk()
+root.title("Завантажувач відео з YouTube")
 
-# Виберіть роздільну здатність відео
-video = youtube.streams.get_highest_resolution()
+# Url
+label_url = tk.Label(root, text="Введіть посилання на відео:")
+label_url.pack()
+entry_url = tk.Entry(root)
+entry_url.pack()
 
-# Виберіть шлях для збереження відео
-save_path = input("Введіть шлях для збереження відео: ")
+# Шлях
+label_path = tk.Label(root, text="Введіть шлях для збереження відео:")
+label_path.pack()
+entry_path = tk.Entry(root)
+entry_path.pack()
 
-# Завантажуємо відео
-video.download(save_path)
+# Кнопка завантаження
+button_download = tk.Button(root, text="Завантажити", command=download_video)
+button_download.pack()
 
-print("Відео успішно завантажено!")
+# Статус
+label_status = tk.Label(root, text="")
+label_status.pack()
+
+root.mainloop()
